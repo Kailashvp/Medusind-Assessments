c2132efee57668c89c36b1fdfe4f26b9c113416a
Kailashvp <92100931+Kailashvp@users.noreply.github.com>
2023-02-28 12:11:09 +0530

    Add files via upload

---
+++ Assignment_Kailash/Assignment (4)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign4
+{
+    public class Person
+    {
+        protected string Name { get; set; }
+        protected string Address { get; set; }
+
+        public Person(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+    public class Doctor : Person
+    {
+        string RegNo { get; set; }
+        double FeesCharge { get; set; }
+        string Specialization { get; set; }
+
+        public Doctor(string name, string address, string regNo, double feesCharge, string specialization)
+            : base(name, address)
+        {
+            RegNo = regNo;
+            FeesCharge = feesCharge;
+            Specialization = specialization;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Address: " + Address);
+            Console.WriteLine("Registration Number: " + RegNo);
+            Console.WriteLine("Fees Charged: " + FeesCharge);
+            Console.WriteLine("Specialization: " + Specialization);
+            Console.Read();
+        }
+    }
+
+    public class doctor
+    {
+        public static void Main()
+        {
+            Console.Write("Enter the name of the Doctor: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the address of the Doctor: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter the registration number of the Doctor: ");
+            string regNo = Console.ReadLine();
+
+            Console.Write("Enter the fees charged by the Doctor: ");
+            double feescharge = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the specialization of the Doctor: ");
+            string specialization = Console.ReadLine();
+
+            Doctor doctor = new Doctor(name, address, regNo, feescharge, specialization);
+            doctor.ShowData();
+        }
+    }
+
+}
---
+++ Assignment_Kailash/Assignment (4)/Scholarship.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignt4
+{
+    class Scholarship
+    {
+        public int totalmarks;
+        public float fees;
+
+        public float Merit(int x, float y)
+        {
+            if (x >= 70 && x <= 80)
+            {
+                return (y * 20) / 100;
+            }
+            else if (x > 80 && x <= 90)
+            {
+                return (y * 30) / 100;
+            }
+            else if (x > 90)
+            {
+                return (y * 50) / 100;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        static void Main(String[] args)
+        {
+            Scholarship sc = new Scholarship();
+
+            Console.WriteLine("Please Enter Total Marks: ");
+            sc.totalmarks = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please Enter Fees: ");
+            sc.fees = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Scholarship Amount is: " + sc.Merit(sc.totalmarks, sc.fees));
+            Console.ReadLine();
+        }
+    }
+
+}
